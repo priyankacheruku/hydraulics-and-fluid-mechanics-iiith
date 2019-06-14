72ab75d2fe9ddeb1dbc1100ecf3606a4015d0143
priyankacheruku <priyankacheruku2016@gmail.com>
2019-06-14 12:19:46 +0530

    value visibility for length

--- src/lab/exp8/SRIP/Codes/js/check.js
+++ src/lab/exp8/SRIP/Codes/js/check.js
@@ -4,6 +4,18 @@ function checkbox() {
   var text = document.getElementById("parameters");
   if (checkBox.checked == true){
     text.style.display = "block";
+
+    var slider = document.getElementById("myRange");
+	var output = document.getElementById("demo");
+	output.innerHTML = slider.value;
+
+	slider.oninput = function() {
+  	output.innerHTML = this.value;
+}
+
+
+
+
   } else {
      text.style.display = "none";
   }
